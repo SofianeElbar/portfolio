b0ac929b0729ef5909c569254b35c692ea8be08d
SofianeElbar <sofiane.elbar@hotmail.fr>
2023-10-26 21:29:10 +0200

    export pdf 26/10/23

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col min-h-screen">
     <SiteHeader />
     <main
-      class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 grow items-start w-full"
+      class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 grow items-start w-full h-full"
     >
       <NuxtPage />
     </main>
---
+++ pages/cv.vue
@@ -0,0 +1,3 @@
+<template>
+  <iframe src="/images/sof_cv.pdf" class="w-full h-full h-screen"></iframe>
+</template>
